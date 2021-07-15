d63c42fa4bf14fd65a7f2b3280e9d042e3e1d74b
Kailey Wong <kaileyswong@gmail.com>
2021-07-15 13:13:56 -0700

    Initial Commit

---
+++ MiniProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Kailey Wong on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
